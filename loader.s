de57fd3a2a4036f060e8cc1ed2b34fed93bf797d
tchamalsha <tharu.chamalsha@gmail.com>
2021-07-23 19:51:39 +0530

    add kmain file

--- loader.s
+++ loader.s
@@ -1,9 +1,15 @@
-global loader                   ; the entry symbol for ELF
+    global loader                   ; the entry symbol for ELF
 
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
+    section .bss
+    align 4                         ; align at 4 bytes
+    kernel_stack:                   ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE      ; reserve stack for the kernel
 
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
@@ -12,6 +18,11 @@ global loader                   ; the entry symbol for ELF
         dd CHECKSUM                 ; and the checksum
 
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+        mov esp, kernel_stack + KERNEL_STACK_SIZE       ; point esp to the start of the
+                                                                                        ;stack (end of memory area)
+        ; The assembly code
+        extern kmain   ; the function sum_of_three is defined elsewhere
+        call kmain       ; call the function, the result will be in eax
+
     .loop:
         jmp .loop                   ; loop forever
