02faf50174873817d12d83649481520ee9491012
tchamalsha <tharu.chamalsha@gmail.com>
2021-08-06 09:44:55 +0530

    writing on kernel

--- io.s
+++ io.s
@@ -10,3 +10,4 @@ mov dx, [esp + 4]    ; move the address of the I/O port into the dx register
 out dx, al           ; send the data to the I/O port
 ret                  ; return to the calling function
 
+
--- kmain.c
+++ kmain.c
@@ -20,12 +20,13 @@ char *fb = (char *) 0x000B8000;
 *  @param c  The character
 *  @param fg The foreground color
 *  @param bg The background color
+*/
 void fb_write_cell(unsigned int i, char c, unsigned char fg, unsigned char bg)
 {
 	fb[i] = c;
 	fb[i + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
 }
-*/
+
 /** fb_move_cursor:
 *  Moves the cursor of the framebuffer to the given position
 *
@@ -38,8 +39,16 @@ void fb_move_cursor(unsigned short pos)
 	outb(FB_COMMAND_PORT, FB_LOW_BYTE_COMMAND);
 	outb(FB_DATA_PORT,    pos & 0x00FF);
 }
+/*void write_sentence(unsigned int l, char *ptr){
+	unsigned int state=0;
+	for(unsigned int i=0;i<l;i++){
+		fb_write_cell(state,ptr[i],FB_GREEN, FB_DARK_GREY);
+		state+=1;
+	}
+	
+}*/
 void kmain(){
-	/*fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);*/
-	fb_move_cursor(00);
+	fb_write_cell(0, 'A', FB_GREEN, FB_DARK_GREY);
+	/*fb_move_cursor(00);*/
 }
 
