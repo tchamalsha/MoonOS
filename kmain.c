581314c67f5480c9ec9a48fcad8e69e2dc9962e7
tchamalsha <tharu.chamalsha@gmail.com>
2021-08-06 13:50:57 +0530

    get outputs

--- kmain.c
+++ kmain.c
@@ -1,4 +1,11 @@
-int kmain()
-{
-	return 0;
+#include "driver/io.h"
+#include "driver/writer.h"
+#include "driver/serial_port.h"
+
+void kmain(){
+	char ptr1[]="Hello! Welcome to MoonOS";
+	char ptr2[]="Hello! I'm Tharushi!";
+	fb_write(24,ptr1);
+	serial_write(0x3F8,ptr2,20);
 }
+
