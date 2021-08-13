d1b70c44b007d12cb6f2518e50298131f95b9564
tchamalsha <tharu.chamalsha@gmail.com>
2021-08-13 18:11:57 +0530

    add segments

--- kmain.c
+++ kmain.c
@@ -1,8 +1,12 @@
 #include "driver/io.h"
 #include "driver/writer.h"
 #include "driver/serial_port.h"
+#include "segmentation/memory_segments.h"
 
 void kmain(){
+
+	segments_install_gdt();	
+	
 	char ptr1[]="Hello! Welcome to MoonOS";
 	char ptr2[]="Hello! I'm Tharushi!";
 	fb_write(24,ptr1);
